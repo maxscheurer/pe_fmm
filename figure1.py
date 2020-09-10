5c158f3e24230e1cf572fac2ecd4e1700a0c60da
Maximilian Scheurer <maximilian.scheurer@iwr.uni-heidelberg.de>
2020-09-10 11:11:27 +0200

    add cppe results, update plots script

--- figure1.py
+++ figure1.py
@@ -30,16 +30,22 @@ sns.set_context("paper")
 
 # TODO: add pelib results!!!
 df_cppe = pd.read_csv("cppe_runs/solvated_50_errors.csv")
+df_pelib = df_cppe.copy()
 
-fig, axes = plt.subplots(nrows=3, ncols=1, sharex=True)
+dfs = [df_cppe, df_pelib]
+impl = ["CPPE", "pelib"]
+
+fig, axes = plt.subplots(nrows=3, ncols=2, sharex=True)
 for ii, theta in enumerate([0.2, 0.3, 0.5, 0.7, 0.99]):
-    for ax, exp_order in zip(axes, [3,5,7]):
-        err_F_i = df_cppe[f"err_F_{theta}_{exp_order}"]
-        sns.histplot(x=np.log10(err_F_i), ax=ax, stat="probability", label=fr"$\theta = {theta}$", color=colors[ii])
-        ax.set_xlabel(r"$\log_{10} F^\mathrm{err}$")
-        ax.set_xlim([-10, 0])
-        ax.set_title(f"order = {exp_order}")
-plt.legend()
+    for axcol, exp_order in zip(axes, [3,5,7]):
+        for ax, df, im in zip(axcol, dfs, impl):
+            err_F_i = df_cppe[f"err_F_{theta}_{exp_order}"]
+            sns.histplot(x=np.log10(err_F_i), ax=ax, stat="probability", label=fr"$\theta = {theta}$", color=colors[ii])
+            ax.set_xlabel(r"$\log_{10} F^\mathrm{err}$")
+            ax.set_xlim([-10, 0])
+            ax.set_title(f"{im}, order = {exp_order}")
+handles, labels = ax.get_legend_handles_labels()
 plt.tight_layout()
-plt.savefig("figure1.png", dpi=600)
+lgd = fig.legend(handles, labels, loc='upper center', ncol=5, bbox_to_anchor=(0.5, 1.05))
+plt.savefig("figure1.png", dpi=600, bbox_extra_artists=(lgd,), bbox_inches="tight")
 
