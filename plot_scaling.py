7f351adc5a5a678c041251fea2fe97b2a9bf06b6
Peter Reinholdt <peter.reinholdt@gmail.com>
2020-09-16 11:29:05 +0200

    Add insets

--- plot_scaling.py
+++ plot_scaling.py
@@ -2,6 +2,8 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid.inset_locator import (inset_axes, InsetPosition,
+                                                  mark_inset)
 import pandas
 
 df_pelib = pandas.read_csv('pelib_data/fmm_timings.csv')
@@ -44,6 +46,22 @@ axes[0,1].set_title('PElib')
 axes[1,1].set_title('PElib (10 nodes)')
 
 axes[0,1].legend(loc=(-1.0,-1.1))
+
+inset = inset_axes(axes[0,1], width=0.7, height=0.7, loc=2, borderpad=2.)
+inset.plot(df_pelib['sites'], 1/3600*df_pelib['direct_t_scf'], '.-', label='SCF')
+inset.plot(df_pelib['sites'], 1/3600*df_pelib['fmm_t_scf'], '.-', label='SCF (FMM)')
+inset.plot(df_pelib['sites'], 1/3600*df_pelib['direct_t_lr'], '.-', label='LR')
+inset.plot(df_pelib['sites'], 1/3600*df_pelib['fmm_t_lr'], '.-', label='LR (FMM)')
+inset.set_ylim(0,4)
+inset.set_xticks([])
+
+inset = inset_axes(axes[1,1], width=0.7, height=0.7, loc=2, borderpad=2.5)
+inset.plot(df_pelib['sites'], 1/3600*df_pelib['direct_10nodes_t_scf'], '.-')
+inset.plot(df_pelib['sites'], 1/3600*df_pelib['fmm_10nodes_t_scf'], '.-')
+inset.plot(df_pelib['sites'], 1/3600*df_pelib['direct_10nodes_t_lr'], '.-')
+inset.plot(df_pelib['sites'], 1/3600*df_pelib['fmm_10nodes_t_lr'], '.-')
+inset.set_ylim(0, 0.4)
+inset.set_xticks([])
 
 plt.savefig('scaling.png')
 plt.show()
